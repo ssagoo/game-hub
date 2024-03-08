96a75eac00e8fc2823023a354b05da34f6d7a02e
ssagoo <sukhdav.sagoo@gmail.com>
2024-03-08 15:55:05 +0000

    Displaying critic score

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -12,7 +12,8 @@ export interface Game {
     name: string;
     rating: number;
     background_image: string;
-    parent_platforms: { platform: Platform }[]
+    parent_platforms: { platform: Platform }[];
+    metacritic: number;
 }
 
 interface FetchGamesResponse {
