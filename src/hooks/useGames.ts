84ad1acb3c0099757455ad818dc5a65689e37ef6
ssagoo <sukhdav.sagoo@gmail.com>
2024-03-08 15:45:49 +0000

    Displaying platform icons

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,11 +1,18 @@
 import { useEffect, useState } from "react";
 import apiClient, { CanceledError } from "../services/api-client";
 
+export interface Platform {
+    id: number;
+    name: string;
+    slug: string;
+}
+
 export interface Game {
     id: number;
     name: string;
     rating: number;
     background_image: string;
+    parent_platforms: { platform: Platform }[]
 }
 
 interface FetchGamesResponse {
